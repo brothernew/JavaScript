ec809e522646386bdb108d71d43284c9258411a1
Thiago Pedrozo <thirp.impacta@gmail.com>
2022-03-20 12:27:31 -0300

    Update

--- Exercicios/Teste.js
+++ Exercicios/Teste.js
@@ -1,12 +1,47 @@
-const chances = [100, 200, 401];
+//=============================================================================
 
-function tripleTheChances( chances ) {
+function retornaNomeCompleto() {
 
-    var resultado = [];
+    var nomeCompleto = 'Thiago Pedrozo';
+    return nomeCompleto;
 
-    for( var i = 0; i < chances.length; i++ ){
-        resultado.push( chances[ i ] * 3 );
-    }return resultado;
+}console.log("Seu nome é: "+retornaNomeCompleto());
 
-}
-console.log(tripleTheChances(chances));
+//=============================================================================
+
+function retornaMinhaIdade() {
+
+    let minhaIdade = 40;
+    return minhaIdade;
+
+}console.log("Sua idade é: "+retornaMinhaIdade()+" anos.");
+
+//=============================================================================
+
+function retornaMinhaNacionalidade() {
+
+    const NACIONALIDADE = 'Brasileira';
+    return NACIONALIDADE;
+
+}console.log("Sua nacionalidade é: "+retornaMinhaNacionalidade());
+
+//=============================================================================
+
+function returnMyExercise() {
+
+    var meuTamanho = 1.86;
+    const PI = 3.14;
+    var corDaMinhaCamisa = "Azul";
+    var queroEstudarNaTrybe = true;
+    var computadorLigado = false;    
+
+    let resultObject = {
+        integer: meuTamanho,
+        float: PI,
+        shirt: corDaMinhaCamisa,
+        trueBoolean: queroEstudarNaTrybe,
+        falseBoolean: computadorLigado
+    }
+
+    return resultObject;
+}console.log(returnMyExercise());
